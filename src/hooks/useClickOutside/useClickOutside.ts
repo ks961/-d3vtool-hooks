71653cfb75bbbf05559bb16b8198e232f92d6672
Sudhanshu Kumar <ksudhanshu1821@gmail.com>
2024-12-21 23:16:21 +0530

    Refactor: changed event listener from target element to document

--- src/hooks/useClickOutside/useClickOutside.ts
+++ src/hooks/useClickOutside/useClickOutside.ts
@@ -22,20 +22,19 @@ export function useClickOutside<T extends HTMLElement>(action: VoidFunction) {
     const targetRef = useRef<T>(null);
 
     function handleOutsideClick(event: React.PointerEvent<T>) {
-        if(!targetRef.current?.contains(event.target as any)) {
+        if (targetRef.current && !targetRef.current.contains(event.target as Node)) {
             action();
         }
     }
 
     useEffect(() => {
-        if(!targetRef.current) return;
 
-        targetRef?.current.addEventListener("click", handleOutsideClick as any);
+        document.addEventListener("click", handleOutsideClick as any);
         
         return () => {
-            targetRef?.current?.addEventListener("click", handleOutsideClick as any);
+            document?.addEventListener("click", handleOutsideClick as any);
         }
-    }, [targetRef.current]);
+    }, []);
 
 
     return targetRef;
