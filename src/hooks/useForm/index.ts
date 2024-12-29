b69266c527647ed2c02a99754d1f62782cdf24c5
Sudhanshu Kumar <ksudhanshu1821@gmail.com>
2024-12-29 08:31:43 +0530

    Patched: 'validator' type issue with its generics and object

--- src/hooks/useForm/index.ts
+++ src/hooks/useForm/index.ts
@@ -105,7 +105,7 @@ export type UseForm<F> = {
  * // Access individual form data
  * const email = getFormData('email');
  */
-export function useForm<FormSchema extends ObjectValidator<any>>(
+export function useForm<FormSchema extends ObjectValidator<Object>>(
     defaultFormData: VInfer<FormSchema>,
     formSchema: FormSchema
 ): UseForm<VInfer<FormSchema>> {
